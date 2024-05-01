071a15583f280088d1f7c7e89a0e2c7bf83f8656
Noah Bergamo <61319498+noah-bergamo@users.noreply.github.com>
2024-05-01 14:22:09 -0300

    Update and rename autowatersystem.ino to humidity-sensor.ino

--- humidity-sensor.ino
+++ humidity-sensor.ino
@@ -9,14 +9,13 @@
 ****************************************/
 
 const char* UBIDOTS_TOKEN = "BBUS-zB1tgVRU5bLNFl4B4GTOcL0twL5J4U";
-const char* WIFI_SSID = "POCO X3 NFC";  // Put here your Wi-Fi SSID
+const char* WIFI_SSID = "POCO X3 NFC";  
 const char* WIFI_PASS = "12345678";
 
 Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
 
 
 void setup() {
-
   Serial.begin(115200);
   pinMode(14, INPUT);
   ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
@@ -27,14 +26,14 @@ void loop() {
   float sensorData = analogRead(0);
 
   uint relativeHumidity = 100.0-(sensorData/1024.0 ) * 100.0;
-  ubidots.add("humidity", relativeHumidity);  // Change for your variable name
+  ubidots.add("humidity", relativeHumidity);
 
 
   bool bufferSent = false;
-  bufferSent = ubidots.send();  // Will send data to a device label that matches the device Id
+  bufferSent = ubidots.send(); 
 
   if (bufferSent) {
-    // Do something if values were sent properly
+
     Serial.println("Values sent by the device");
   }
   delay(1000);
